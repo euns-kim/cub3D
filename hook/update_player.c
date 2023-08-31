7b86a244f3e4f62542edbd7be7fc93b2e094d150
Sangyao42 <wangsangyao@hotmail.com>
2023-08-31 15:23:26 +0200

    Fixed[hook]: wall collision - the movement WSAD need to be calculate correct

--- hook/update_player.c
+++ hook/update_player.c
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 21:42:38 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/30 18:27:35 by sawang           ###   ########.fr       */
+/*   Updated: 2023/08/31 15:21:48 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,28 @@
 void	update_hook_input(t_player player, t_input *input, keys_t key)
 {
 	if (key == MLX_KEY_RIGHT)
-		input->angle += 1 * PI / 180;
+		input->angle = -1 * PI / 180;
 	if (key == MLX_KEY_LEFT)
-		input->angle += -1 * PI / 180;
+		input->angle = 1 * PI / 180;
 	if (key == MLX_KEY_W)
-	{
-		input->hori_delta = -cos(player.dir_modif) * STEP;
-		input->vert_delta = sin(player.dir_modif) * STEP;
-	}
-	if (key == MLX_KEY_S)
 	{
 		input->hori_delta = cos(player.dir_modif) * STEP;
 		input->vert_delta = -sin(player.dir_modif) * STEP;
 	}
+	if (key == MLX_KEY_S)
+	{
+		input->hori_delta = -cos(player.dir_modif) * STEP;
+		input->vert_delta = -sin(player.dir_modif) * STEP;
+	}
 	if (key == MLX_KEY_A)
 	{
-		input->hori_delta = sin(player.dir_modif + PI / 2) * STEP;
-		input->vert_delta = -cos(player.dir_modif + PI / 2) * STEP;
+		input->hori_delta = cos(player.dir_modif + PI / 2) * STEP;
+		input->vert_delta = sin(player.dir_modif + PI / 2) * STEP;
 	}
 	if (key == MLX_KEY_D)
 	{
-		input->hori_delta = -sin(player.dir_modif - PI / 2) * STEP;
-		input->vert_delta = cos(player.dir_modif - PI / 2) * STEP;
+		input->hori_delta = cos(player.dir_modif - PI / 2) * STEP;
+		input->vert_delta = -sin(player.dir_modif - PI / 2) * STEP;
 	}
 }
 
@@ -46,19 +46,21 @@ void	update_player(t_player *player, t_input *input)
 {
 	t_vec	temp_pos;
 
-	player->dir_modif = player->dir + input->angle;
+	player->dir_modif = player->dir_modif + input->angle;
 	if (player->dir_modif > 2 * PI)
 		player->dir_modif -= 2 * PI;
 	if (player->dir_modif < 0)
 		player->dir_modif += 2 * PI;
 	temp_pos.x = player->pos_modif.x + input->hori_delta;
 	temp_pos.y = player->pos_modif.y + input->vert_delta;
-	if (worldMap[(int)(temp_pos.y / TILE_SIZE)][(int)(player->pos_modif.x / TILE_SIZE)] != '1')
+	if ((int)(temp_pos.y / TILE_SIZE) < mapHeight && \
+		(int)(temp_pos.y / TILE_SIZE) >= 0 && \
+		(int)(temp_pos.x / TILE_SIZE) < mapWidth && \
+		(int)(temp_pos.x / TILE_SIZE) >= 0 && \
+		worldMap[(int)(temp_pos.y / TILE_SIZE)][(int)(temp_pos.x / TILE_SIZE)] != 1)
 	{
 		player->pos_modif.x = temp_pos.x;
 		player->pos_modif.y = temp_pos.y;
 	}
-	// init_input(input);
-	input->hori_delta = 0;
-	input->vert_delta = 0;
+	init_input(input);
 }
